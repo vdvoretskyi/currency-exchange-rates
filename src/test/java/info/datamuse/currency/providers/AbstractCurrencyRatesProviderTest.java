f43657b2a5ae9390e3fa679d95ea02c587e3faad
Alex Shesterov <alex.shesterov@gmail.com>
2018-07-18 13:16:32 +0000

    common test base for providers

--- src/test/java/info/datamuse/currency/providers/AbstractCurrencyRatesProviderTest.java
+++ src/test/java/info/datamuse/currency/providers/AbstractCurrencyRatesProviderTest.java
@@ -1,6 +1,7 @@
 package info.datamuse.currency.providers;
 
 import info.datamuse.currency.CurrencyRatesProvider;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.math.BigDecimal;
@@ -9,6 +10,8 @@ import java.util.Currency;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.greaterThan;
 import static org.hamcrest.Matchers.is;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public abstract class AbstractCurrencyRatesProviderTest {
 
@@ -23,12 +26,34 @@ public abstract class AbstractCurrencyRatesProviderTest {
     }
 
     @Test
+    public void testGetExchangeRateIllegalArguments() {
+        final CurrencyRatesProvider currencyRatesProvider = getCurrencyRatesProvider();
+
+        assertThrows(IllegalArgumentException.class, () -> currencyRatesProvider.getExchangeRate("", "EUR"));
+        assertThrows(IllegalArgumentException.class, () -> currencyRatesProvider.getExchangeRate("USD", ""));
+        assertThrows(IllegalArgumentException.class, () -> currencyRatesProvider.getExchangeRate("", ""));
+        assertThrows(IllegalArgumentException.class, () -> currencyRatesProvider.getExchangeRate("E", "EUR"));
+        assertThrows(IllegalArgumentException.class, () -> currencyRatesProvider.getExchangeRate("USD", "USDX"));
+        assertThrows(IllegalArgumentException.class, () -> currencyRatesProvider.getExchangeRate("U?!", "UAH"));
+        assertThrows(IllegalArgumentException.class, () -> currencyRatesProvider.getExchangeRate("SEK", "SE1"));
+        assertThrows(IllegalArgumentException.class, () -> currencyRatesProvider.getExchangeRate("S_K", "RUB"));
+
+        assertThrows(NullPointerException.class, () -> currencyRatesProvider.getExchangeRate(null, "EUR"));
+        assertThrows(NullPointerException.class, () -> currencyRatesProvider.getExchangeRate("USD", null));
+        assertThrows(NullPointerException.class, () -> currencyRatesProvider.getExchangeRate((String) null, null));
+        assertThrows(NullPointerException.class, () -> currencyRatesProvider.getExchangeRate(null, Currency.getInstance("EUR")));
+        assertThrows(NullPointerException.class, () -> currencyRatesProvider.getExchangeRate(Currency.getInstance("USD"), null));
+        assertThrows(NullPointerException.class, () -> currencyRatesProvider.getExchangeRate((Currency) null, null));
+    }
+
+    @Test
     public void testGetExchangeRateByCurrencyCodes() {
         final CurrencyRatesProvider currencyRatesProvider = getCurrencyRatesProvider();
         final BigDecimal exchangeRate = currencyRatesProvider.getExchangeRate(
             getSourceCurrencyCode(),
             getTargetCurrencyCode()
         );
+        assertNotNull(exchangeRate);
         assertThat(exchangeRate, is(greaterThan(BigDecimal.ZERO)));
     }
 
@@ -39,6 +64,7 @@ public abstract class AbstractCurrencyRatesProviderTest {
             Currency.getInstance(getSourceCurrencyCode()),
             Currency.getInstance(getTargetCurrencyCode())
         );
+        assertNotNull(exchangeRate);
         assertThat(exchangeRate, is(greaterThan(BigDecimal.ZERO)));
     }
 
